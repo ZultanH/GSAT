da6c8670dd46ca4a1cf7a93d6a80b364ea98ccc6
Zultan <43057246+ZultanH@users.noreply.github.com>
2019-11-02 13:37:35 +1000

    Update Base.py

--- models/Base.py
+++ models/Base.py
@@ -9,4 +9,4 @@ class BaseModel(Model):
     date_modified   = DateTimeField(default = datetime.datetime.now())
 
     class Meta:
-        database = connect("postgres://jtfahlfstizxxv:66f734d3134489f0b9ec0ed32f0be69a24f60a25d45a539889fc6c71aab3e80a@ec2-107-20-185-16.compute-1.amazonaws.com:5432/d7q25b18db4ht8")
+        database = connect("")
