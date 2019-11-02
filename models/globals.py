5101779efb60ea9287f739ef8b9f3edcab587345
Zultan <43057246+ZultanH@users.noreply.github.com>
2019-11-02 13:38:43 +1000

    Update globals.py

--- models/globals.py
+++ models/globals.py
@@ -21,6 +21,6 @@ class JWTStatus(Enum):
     INVALID = 2
     OTHER   = 3
 
-db = connect("postgres://jtfahlfstizxxv:66f734d3134489f0b9ec0ed32f0be69a24f60a25d45a539889fc6c71aab3e80a@ec2-107-20-185-16.compute-1.amazonaws.com:5432/d7q25b18db4ht8")
+db = connect("")
 db.connect()
 db.create_tables([User, Role, BannedUser, Notification, Permission, Server, Stub, Loader])
